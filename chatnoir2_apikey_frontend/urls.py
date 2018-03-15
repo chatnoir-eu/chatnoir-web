d92877802db062abe816162ecc53d899130ab19b
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2018-03-15 18:11:05 +0100

    Use apikey/ URL prefix

--- chatnoir2_apikey_frontend/urls.py
+++ chatnoir2_apikey_frontend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('frontend.urls')),
+    path('apikey/admin/', admin.site.urls),
+    path('apikey/', include('frontend.urls')),
 ]
