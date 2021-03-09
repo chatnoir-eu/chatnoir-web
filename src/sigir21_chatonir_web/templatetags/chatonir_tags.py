b6ab83e7b01adf0fcbfa5f7a5b4f3df35d3e5d32
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-03-09 14:36:56 +0100

    Query full name when clicking author in result list

--- src/sigir21_chatonir_web/templatetags/chatonir_tags.py
+++ src/sigir21_chatonir_web/templatetags/chatonir_tags.py
@@ -10,14 +10,16 @@ register = template.Library()
 
 @register.filter(name='author_list')
 def author_list(authors, link_targets=False):
-    authors = [a.split(' ')[-1] for a in authors]
+    authors_last = [a.split(' ')[-1] for a in authors]
 
     if link_targets:
         link_base = f'{reverse("search:index")}?q=author%3A'
-        authors = [f'<a href="{link_base + url_quote(a.lower())}">{html_escape(a)}</a>' for a in authors]
+        authors_last = [
+            f'<a href="{"".join((link_base, "%22", url_quote(authors[i].lower()), "%22"))}">{html_escape(al)}</a>'
+            for i, al in enumerate(authors_last)]
 
-    if 1 <= len(authors) <= 2:
-        return ' and '.join(authors)
-    elif len(authors) > 2:
-        return f'{authors[0]} et al.'
+    if 1 <= len(authors_last) <= 2:
+        return ' and '.join(authors_last)
+    elif len(authors_last) > 2:
+        return f'{authors_last[0]} et al.'
     return 'unknown'
