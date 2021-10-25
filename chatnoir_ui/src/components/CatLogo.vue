49c09dbe4ea9a8230ffa71117e3e417989be5180
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2021-10-25 17:59:40 +0200

    Always embed SVG

--- chatnoir_ui/src/components/CatLogo.vue
+++ chatnoir_ui/src/components/CatLogo.vue
@@ -1,24 +1,22 @@
 <template>
-<span :class="$style['chatnoir-logo']" aria-hidden="true" role="img">
-    <object ref="chatNoirLogo" type="image/svg+xml"
-            :data="require('@/assets/img/chatnoir.svg').default" @load="init()">
-        <img src="@/assets/img/chatnoir.svg" alt="ChatNoir Logo">
-    </object>
+<span ref="chatNoirLogoContainer" :class="$style['chatnoir-logo']" aria-hidden="true" role="img">
+    <inline-svg :src="require('@/assets/img/chatnoir.svg').default" @loaded="logoElement = $event" />
 </span>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import InlineSvg from 'vue-inline-svg';
 
 const purrTimeout = ref(null)
-const chatNoirLogo = ref(null)
+const logoElement = ref(null)
 
 function purr() {
     if (purrTimeout.value !== null) {
         clearTimeout(purrTimeout.value)
     }
 
-    const eyes = chatNoirLogo.value.querySelector('#Eyes')
+    const eyes = logoElement.value.querySelector('#Eyes')
     eyes.setAttribute('visibility', 'hidden')
 
     purrTimeout.value = setTimeout(() => {
@@ -27,31 +25,9 @@ function purr() {
     }, 300)
 }
 
-// Fetch and embed cat as inline SVG to circumvent CORS issues with accessing contentDocument
-async function embedCat(element) {
-    const result = await fetch(element.getAttribute('data'))
-    const tmp = document.createElement('div')
-    tmp.innerHTML = await result.text()
-    chatNoirLogo.value = tmp.querySelector('svg')
-    element.replaceWith(chatNoirLogo.value)
-}
-
-function attachEvents() {
-    chatNoirLogo.value.querySelector('#Body').addEventListener('mousemove', purr)
-}
-
-function init() {
-    if (chatNoirLogo.value.tagName.toLowerCase() === 'object') {
-        if (chatNoirLogo.value.contentDocument === null) {
-            // CORS issue
-            embedCat(chatNoirLogo.value).then(() => attachEvents())
-            return
-        }
-        chatNoirLogo.value = chatNoirLogo.value.contentDocument
-    }
-
-    attachEvents()
-}
+watch(logoElement, () => {
+    logoElement.value.querySelector('#Body').addEventListener('mousemove', purr)
+})
 
 defineExpose({
     purr
@@ -61,8 +37,8 @@ defineExpose({
 <style module>
 .chatnoir-logo {
     object, svg, img {
-        @apply h-40;
         @apply inline-block;
+        @apply h-full w-auto max-w-full;
     }
 }
 </style>
