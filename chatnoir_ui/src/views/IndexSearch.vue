35653679036784005f5950cc08a1aa38e7201e93
Janek Bevendorff <janek@jbev.net>
2021-11-02 21:09:32 +0100

    Revert keep-alive change

--- chatnoir_ui/src/views/IndexSearch.vue
+++ chatnoir_ui/src/views/IndexSearch.vue
@@ -3,9 +3,7 @@
     Renders either the index or search view depending on whether a search query is given.
 -->
 <template>
-<keep-alive>
-    <component :is="$route.query.q ? Search : Index" />
-</keep-alive>
+<component :is="$route.query.q ? Search : Index" />
 </template>
 
 <script setup>
