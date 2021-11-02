ac2c36241603f755c5fae2b1fefbced81d2a5ba1
Janek Bevendorff <janek@jbev.net>
2021-11-02 20:56:59 +0100

    Add keep-alive

--- chatnoir_ui/src/views/IndexSearch.vue
+++ chatnoir_ui/src/views/IndexSearch.vue
@@ -3,7 +3,9 @@
     Renders either the index or search view depending on whether a search query is given.
 -->
 <template>
-<component :is="$route.query.q ? Search : Index" />
+<keep-alive>
+    <component :is="$route.query.q ? Search : Index" />
+</keep-alive>
 </template>
 
 <script setup>
