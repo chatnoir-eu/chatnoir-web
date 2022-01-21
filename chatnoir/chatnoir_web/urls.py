8b7ac4df271dc9c6e1299dad71f7372c2fc5047c
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2022-01-21 16:02:18 +0100

    Remove doc route

--- chatnoir/chatnoir_web/urls.py
+++ chatnoir/chatnoir_web/urls.py
@@ -21,7 +21,5 @@ app_name = 'chatnoir_web'
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'search', views.search, name='search'),
-    path(r'cache', views.cache, name='cache'),
-    path(r'doc/', views.doc, name='doc'),
-    re_path(r'doc/(?P<subpath>.+)/$', views.doc, name='doc-sub')
+    path(r'cache', views.cache, name='cache')
 ]
