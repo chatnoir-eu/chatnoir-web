3b55831a7db1276b6f3ada327442ea7381756f0b
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2022-01-25 16:45:14 +0100

    Remove or override unnecessary config options

--- chatnoir/web_cache/settings.py
+++ chatnoir/web_cache/settings.py
@@ -5,9 +5,6 @@ Django settings for the ChatNoir Web Cache.
 from chatnoir.settings import *
 
 APPLICATION_NAME = 'ChatNoir Web Cache'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ALLOWED_HOSTS = ['*']
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 INSTALLED_APPS = [
@@ -23,9 +20,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
-
-# Do not filter console logs in production mode
-DEFAULT_LOGGING['handlers']['console']['filters'] = []
 
 ROOT_URLCONF = 'web_cache.urls'
 
@@ -50,36 +44,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'web_cache.wsgi.application'
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-USE_THOUSAND_SEPARATOR = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'chatnoir_static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '..', 'chatnoir_ui', 'dist'),
-    os.path.join(BASE_DIR, 'web_cache', 'static')
-]
-
-# Webpack loader
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'CACHE': not DEBUG,
-        'STATS_FILE': os.path.join(BASE_DIR, '..', 'chatnoir_ui', 'webpack-stats.json'),
-        'POLL_INTERVAL': 0.1,
-        'IGNORE': [r'.+\.hot-update.js', r'.+\.map'],
-    }
+DATABASES = {
+    'default': {}
 }
+
+STATICFILES_DIRS = []
 
 try:
    from .local_settings import *
