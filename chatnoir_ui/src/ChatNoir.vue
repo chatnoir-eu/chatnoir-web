f0a3cc6779c01c6d3df80ebf324add1d09bea5c5
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2025-05-14 10:24:12 +0200

    Change footer links depending on loaded backend app

--- chatnoir_ui/src/ChatNoir.vue
+++ chatnoir_ui/src/ChatNoir.vue
@@ -3,22 +3,31 @@
     <main class="flex-grow w-full">
         <router-view :key="$route.path" />
     </main>
-
-    <webis-footer
-        copyright-start-year="2012"
-        contact-fragment="#bevendorff"
-        :additional-links="[
-            {href: 'https://webis.de/publications.html?q=bevendorff_2018', anchor: 'Cite'},
-            {href: $router.resolve({name: 'ApikeyRequest'}), anchor: 'Request API Key'},
-            {href: $router.resolve({name: 'Docs'}).fullPath, anchor: 'API Documentation'},
-        ]"
-    />
+    <webis-footer copyright-start-year="2012" contact-fragment="#bevendorff" :additional-links="getFooterLinks()" />
 </section>
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import WebisFooter from '@/components/WebisFooter.vue'
+
+const router = useRouter()
+
+function getFooterLinks() {
+    const links = [
+        {href: 'https://webis.de/publications.html?q=bevendorff_2018', anchor: 'Cite'},
+    ]
+    if (window._APP_SETTINGS.app_module === 'chatnoir') {
+        links.push(
+            {href: router.resolve({name: 'ApikeyRequest'}), anchor: 'Request API Key'},
+            {href: router.resolve({name: 'Docs'}), anchor: 'API Documentation'}
+        )
+    } else {
+        links.push({href: 'https://www.chatnoir.eu/', anchor: 'ChatNoir'})
+    }
+    return links
+}
 
 onMounted(() => {
     // Hide browser address bar
